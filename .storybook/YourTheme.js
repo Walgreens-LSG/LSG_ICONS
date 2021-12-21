e7f1938b298e7ce0089bad1f4b54dbf5ea5b298d
Chamble <AmirahChamble@users.noreply.github.com>
2021-12-21 15:32:13 -0500

    Updated Theme images

--- .storybook/YourTheme.js
+++ .storybook/YourTheme.js
@@ -1,11 +1,12 @@
 import {create} from '@storybook/theming';
+import {logoUrl} from '../public/logo-mark.svg';
 
 export default create({
     base: 'light',
 
     brandTitle: 'Walgreens\'s | Live Style Guide Icon Library',
     brandUrl: "https://walgreens.com",
-    brandImage: "../full-logo.png",
+    brandImage: (process.env.NODE_ENV === 'production') ? logoUrl : "/logo-mark.svg",
 
     //Typography
     fontBase:'"Source Sans Pro", sans-serif',
